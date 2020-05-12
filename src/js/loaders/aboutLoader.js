914cc696eee21ea848d5c52a4b1cd74eacf31861
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2020-05-12 19:11:07 +0300

    fix layouts

--- src/js/loaders/aboutLoader.js
+++ src/js/loaders/aboutLoader.js
@@ -12,9 +12,9 @@ const aboutLoader = () => {
   const tl = new TimelineMax()
 
   tl
-    .to(h1, 0.1, {opacity: 1, ease: Power3.easeInOut}, 0, 0.2)
+    .to(h1, 0.1, { opacity: 1, ease: Power3.easeInOut }, 0, 0.2)
     .staggerTo(h1.querySelectorAll('.splitted-line'), 0.8, { y: 0, ease: Expo.easeOut }, 0.2)
-    .to(content, 1, {opacity: 1, ease: Power1.easeOut})
+    .to(content, 1, { opacity: 1, ease: Power1.easeOut })
 }
 
 export default aboutLoader
